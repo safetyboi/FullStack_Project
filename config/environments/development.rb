3a0e257da80bcdcf0bc341e280c03b94db6b90bc
Daniel Culbertson <daniel.j.culbertson@gmail.com>
2022-11-07 10:26:01 -0800

    aws setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  config.active_storage.service = :amazon_dev
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  
+  config.active_storage.service = :amazon_prod
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
